89b81688d0dcb0cfea430a8867fed09b9e5c8d66
JdeGuise <degui1jr@cmich.edu>
2016-01-29 11:24:58 -0500

    Adds a getPID() method that uses bash to find PID

--- CPS 470 Homework 2/src/CollatzMain.java
+++ CPS 470 Homework 2/src/CollatzMain.java
@@ -1,33 +1,57 @@
+import java.awt.List;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.Vector;
 public class CollatzMain {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
 		int choice = 0;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter one integer as input");
+		
 		try{
 			choice = input.nextInt();
+			getPID();
+
+			String[] command = {"bash", "fork()", Integer.toString(choice)};
+	        Process p = Runtime.getRuntime().exec(command);
+			long one = System.currentTimeMillis();
+			CollatzCalcs.collatzComp(choice);			
+			long two = System.currentTimeMillis();
+			long three = two - one;
+			System.out.println(three + "ms to perform calculations.");
+			input.close();
 		}
+		
 		catch(InputMismatchException ime){
 			ime.printStackTrace();
 		}
-		List<V> command = {"bash", choice};
-
-        Process p = new ProcessBuilder(command).start();
-		long one = System.currentTimeMillis();
-		
-		collatzComp(choice);
-		
-		long two = System.currentTimeMillis();
-		
-		long three = two - one;
-		System.out.println(three + "ms to perform calculations.");
-		input.close();
 	}
+	
+    public static String getPID() throws IOException, InterruptedException{
+    	Vector<String> commands = new Vector<String>();
+    	
+    	commands.add("/bin/bash");
+    	commands.add("-c");
+    	commands.add("echo $PPID");
+    	ProcessBuilder pb = new ProcessBuilder(commands);
+    	
+    	Process pr = pb.start();
+    	pr.waitFor();
+    	
+    	if(pr.exitValue() == 0){
+    		BufferedReader outReader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+    		return outReader.readLine().trim();
+    	}
+    	else{
+    		System.out.println("Error while getting PID");
+    		return "";
+    	}
+    }
 	
 	
 	
