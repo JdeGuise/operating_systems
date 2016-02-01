caa96cf152cb8a977cc1a1de6123ced65c1852fa
Jon Ragon <jonragon@AUX-db6da5.local>
2016-02-01 14:06:18 -0500

    Tentative Final HaikuServer

--- HaikuServer/src/HaikuServer.java
+++ HaikuServer/src/HaikuServer.java
@@ -1,25 +1,37 @@
 import java.io.*;
 import java.net.*;
-import java.util.Date;
+
+import java.util.*;
 
 import javafx.application.*;
 
 public class HaikuServer extends Thread  {
-	
-	public void start(){
+	static Map<Integer, String> haikuList = new HashMap<Integer, String>();
+	static int count;
+
+	public static void main(String argv[]) throws FileNotFoundException {
+		renderHaikuList();
 		Thread thread = new Thread() { 
 			@Override
 			public void run(){ 
 			try {
 				ServerSocket serverSocket = new ServerSocket(5575);
-				Platform.runLater(() -> System.out.println(new Date() + ": Server started at socket 5575"));
+				System.out.println(new Date() + ": Server started at socket 5575");
 				
 				//listen for connection
 				while (true) {
+					Socket haikuRequest = serverSocket.accept();
 					
+					PrintWriter pout = new PrintWriter((haikuRequest).getOutputStream(), true);//writeInt was documented)
+					String haiku = returnHaiku();
+					pout.println(haiku);//prints a haiku
+					System.out.println(new Date() + ": Server provided\n" + haiku + "to client. Resetting for new connection.");
+					
+					haikuRequest.close();
 				}
 				
 			} catch (IOException ex) {
+				System.out.println("testttt");
 					ex.printStackTrace();
 				
 			}
@@ -28,6 +40,24 @@ public class HaikuServer extends Thread  {
 			}
 		};
 		thread.start();
+	}
+	
+	public static void renderHaikuList() throws FileNotFoundException{
+		Scanner haikusIn = new Scanner(new FileReader("haikus.txt"));
+		count = 0;
+		while(haikusIn.hasNextLine()){
+			String haiku = "\t" + haikusIn.nextLine() + "\n\t" + haikusIn.nextLine() + "\n\t" + haikusIn.nextLine() + "\n";
+			haikuList.put(count, haiku);
+
+			count++;
+		}
+		haikusIn.close();
+	}
+	public static String returnHaiku(){
+		int range = count;
+		int selectedHaiku = (int) ((Math.random()) * count);
+		return haikuList.get(selectedHaiku);
+		
 	}
 	
 }
