32f61e4d83925167f099b4083f93050258c09766
JdeGuise <degui1jr@cmich.edu>
2016-02-07 17:04:43 -0500

    renamed variables to be less awful and sporadic

--- CPS 470 Homework_2_Pt3/src/Piping.java
+++ CPS 470 Homework_2_Pt3/src/Piping.java
@@ -9,23 +9,23 @@ public class Piping {
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		File initial_input = new File("input.txt");
-		String input = "";			
+		File sc_input_file = new File("input.txt");
+		String scanner_input = "";			
 
 		try {
 			PrintWriter pw = new PrintWriter(initial_input);
 
 			System.out.println("Enter phrase to be reverse cased");
 						
-			input = sc.nextLine();
+			scanner_input = sc.nextLine();
 			
 			//writing argument to a file
-			pw.write(input);
+			pw.write(scanner_input);
 			pw.close();
 			
 			//building process command based on bash shell, string flag, file type
 			//piped catted input goes to translate modifiers to swap case
-			String[] cmd = {"/bin/sh", "-c", "cat " + initial_input + " | tr 'a-zA-Z' 'A-Za-z'"};
+			String[] cmd = {"/bin/sh", "-c", "cat " + sc_input_file + " | tr 'a-zA-Z' 'A-Za-z'"};
 
 			
 			ProcessBuilder pb = new ProcessBuilder(cmd);
