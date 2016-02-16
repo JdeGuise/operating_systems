23f1bf58b2f95bf0358c22c59b523efdf5fd8781
JdeGuise <degui1jr@cmich.edu>
2016-02-16 13:28:17 -0500

    Updated documentation for part 2 and part 3

--- CPS 470 Homework 3/src/Part2_30Pts.java
+++ CPS 470 Homework 3/src/Part2_30Pts.java
@@ -1,4 +1,4 @@
-
+//Part2_30Pts.java, John deGuise/Jon Ragon, CPS 470 2-3:15 T/TH, Due 2/25 by 11:59PM
 public class Part2_30Pts {
 
 	public static void main(String[] args) {
--- CPS 470 Homework 3/src/Part3_40Pts.java
+++ CPS 470 Homework 3/src/Part3_40Pts.java
@@ -1,4 +1,4 @@
-
+//Part3_40Pts.java,  John deGuise/Jon Ragon, CPS 470 2-3:15 T/TH, Due 2/25 by 11:59PM
 public class Part3_40Pts {
 
 	public static void main(String[] args) {
