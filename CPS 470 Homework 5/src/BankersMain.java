f75f73bf382c69dcdd62ae7a2e66c25dfc857b30
johndeguise <degui1jr@cmich.edu>
2016-04-07 17:57:08 -0400

    initializes homework 5

---
+++ CPS 470 Homework 5/src/BankersMain.java
@@ -0,0 +1,10 @@
+//BankersMain.java, John deGuise/Jon Ragon, CPS 470, 4/18/2016 @ midnight
+
+public class BankersMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
