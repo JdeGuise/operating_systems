d652e3b10c6194619dee71d5977ce4571c9c0d6f
Jon Ragon <jonragon@AUX-db6da5.local>
2016-04-18 17:24:44 -0400

    for review - incomplete yet

--- CPS 470 Homework 5/src/BankersMain.java
+++ CPS 470 Homework 5/src/BankersMain.java
@@ -1,27 +1,80 @@
 //BankersMain.java, John deGuise/Jon Ragon, CPS 470, 4/18/2016 @ midnight
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.Semaphore;
 
-public class Student {
+// public class Customer {
 
-}
+// }
 
 public class BankersMain {
-	private int need[][];
-	private int allocate[][];
-	private int max[][];
-	private int avail[];
-	private int numProcesses = 5;
-	private int numResources = 3;
+	private static int need[][];
+	private static int allocate[][];
+	private static int max[][];
+	private static int avail[];
+	private static int numProcesses = 5;
+	private static int numResources = 3;
 	private static String[] arguments;
+
+	private final Semaphore mutex = new Semaphore(1, true);
 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		arguments = args;
 		input();
-		// TODO create student objects based on numProcesses and activate them in the thread numerically from 0 to 4
 
+		Runnable bankThread = new Runnable() {
+			public void run() {
+				long threadID = Thread.currentThread().getId() - 10;
+				System.out.println(threadID);
+				while (true) {
+					//do what it should do, which is accepting and releasing resources
+					//if system is in a safe state
+					//in the order of processes
+
+					mutex.acquire();
+					//do the thing
+					int request[] = new int[numProcesses]
+					for(int i = 0; i > numProcesses; i++){
+						request[i] = (int)(Math.random() * need[threadID][i])
+					}
+
+					requestResources(threadID, request[]);
+
+					mutex.release();
+
+
+					try {
+					    Thread.sleep(2000);
+					} 
+					catch(InterruptedException e){
+					     
+					}
+
+					mutex.acquire();
+					releaseResources(threadID, request[]);
+					mutex.release();
+
+
+					try {
+					    Thread.sleep(2000);
+					} 
+					catch(InterruptedException e){
+					     
+					}
+
+				}
+			}
+		};
+		// TODO create student objects based on numProcesses and activate them in the thread numerically from 0 to 4
+		int threads = 0;
+		for(int i = 0; i < numProcesses; i++){
+			new Thread(bankThread).start();
+		}
+		// while (threads > numProcesses){
+		// 	new Thread(bankThread).start();
+		// }
 
 		
 		calculate_need();
@@ -30,14 +83,16 @@ public class BankersMain {
 	}
 
 	public int requestResources (int customer_number, int request[]) {
-		//
+		//needs to check if request leaves system in safe state
+		return 0;
 	}
 
 	public int releaseResources (int customer_number, int release[]) {
 
+		return 0;
 	}
 	
-	private int[][] calculate_need (){
+	private static int[][] calculate_need (){
 		for(int i = 0; i < numProcesses; i++){
 			for (int j = 0; j < numResources; j++){
 				need[i][j] = max[i][j] - allocate[i][j];
@@ -57,7 +112,7 @@ public class BankersMain {
 		return true;
 	}
 	
-    private void input(){
+    private static void input(){
         Scanner sc = new Scanner(System.in);
         // System.out.println("Enter number of processes: ");
         // numProcesses = sc.nextInt();
@@ -70,28 +125,26 @@ public class BankersMain {
         allocate = new int[numProcesses][numResources];
         avail = new int[numResources];
 
-        // for(int i = 0; i < numProcesses; i++){
-        // 	allocate[i] = Integer.parseInt(arguments[i]);
-        // }
-        //System.out.println(allocate);
          
-        //System.out.println("Enter allocations for proccesses:");
-        for(int i = 0;i < numProcesses; i++){
-         	System.out.println("Current Process: " + i);
+        // System.out.println("Enter allocations for proccesses:");
+        // for(int i = 0;i < numProcesses; i++){
+        //  	System.out.println("Current Process: " + i);
         
-            for(int j = 0; j < numResources; j++){
-            	allocate[i][j] = sc.nextInt();  //allocation matrix
-            }
-        }     
+        //     for(int j = 0; j < numResources; j++){
+        //     	allocate[i][j] = sc.nextInt();  //allocation matrix
+        //     }
+        // }     
         
           
-       //  System.out.println("Enter max matrix -->");
-       //  for(int i = 0; i < numProcesses; i++){
-       //       for(int j = 0; j < numResources; j++){
-       //      	 max[i][j] = sc.nextInt();  //max matrix
-       //       }
-       //  }
+        // System.out.println("Enter max matrix -->");
+        // for(int i = 0; i < numProcesses; i++){
+        //      for(int j = 0; j < numResources; j++){
+        //     	 max[i][j] = sc.nextInt();  //max matrix
+        //      }
+        // }
         
+
+        //Sets available
         for(int i = 0; i < numResources; i++){
         	avail[i] = Integer.parseInt(arguments[i]);
         }
